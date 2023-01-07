e609110de0e18c2af293b7d42c774344ca74521b
Natasha <natasha.khoe@gmail.com>
2023-01-07 17:59:38 +1100

    commit tree a128975bad35f4879be87944169f6fb5701638aa

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,19 @@
+# require 'nokogiri'
+# require 'open-uri'
+
+# html = open("https://flatironschool.com/")
+# # gets the html file and parse it into nodes including the css
+
+# doc = Nokogiri::HTML(html)
+
+# puts doc.css(".heading-primary color-white mb-20 text-shadow animate animate-1s animated fadeInUp").text
+
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
